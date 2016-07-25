cc8472c8b4e51677be6e9fe31f79529c4937cf60
DJEISON LUÍS COSTA PINHEIRO SANTOS <15160975@cfp127aluno.local>
2016-07-25 16:14:07 -0300

    Segundo commit

--- src/br/com/contatos/controller/ContatoController.java
+++ src/br/com/contatos/controller/ContatoController.java
@@ -22,16 +22,16 @@ public class ContatoController implements Initializable{
 	@FXML TextField txtNome;
 	@FXML Button btnInserir;
 	@FXML ListView lstContatos;
-	
+
 	private void preencherLista(){
-		
+
 		lstContatos.getItems().clear();
 		Connection con = MySqlConnect.ConectarDb();
-		
+
 		String sql = "SELECT * FROM  contact;";
 		try {
 			ResultSet rs =	con.createStatement().executeQuery(sql);
-			
+
 			while(rs.next()){
 				String nome = rs.getString("name");
 				String telefone = rs.getString("phone");
@@ -42,38 +42,39 @@ public class ContatoController implements Initializable{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-	} 
+
+
+	}
 	@FXML public void InserirContatos() {
-		
+
 		Connection con = MySqlConnect.ConectarDb();
-		
-		
+
+
 		String sql = "insert into contact (name, phone) values(?,?);";
-		
+
 		PreparedStatement parametros;
-		
+
 		try {
 			parametros = con.prepareStatement(sql);
 			parametros.setString(1,txtNome.getText());
 			parametros.setString(2,txtTelefone.getText());
-			
+
 			parametros.executeUpdate();
-			
+
 			con.close();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
+
+
+
 	}
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
+		//metodo para preencher a lista
 		preencherLista();
 	}
-	
+
 }
